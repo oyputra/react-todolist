a9f2a2ef7d251532b0d5ea50352947b4035fe694
Oktavian Yudistira Putra <oktavianyudistiraputra@gmail.com>
2021-09-20 17:40:16 +0700

    create todolist page & todo component

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,35 +1,46 @@
 import React from "react";
 
+import Todo from "./Todo";
+
 const Todos = () => {
+  const todos = [
+    {
+      text: "Belajar React"
+    },
+    {
+      text: "Belajar props di React"
+    },
+    {
+      text: "Belajar state di React"
+    },
+    {
+      text: "Belajar React"
+    },
+    {
+      text: "Belajar React"
+    },
+    {
+      text: "Belajar React"
+    },
+    {
+      text: "Belajar props di React"
+    },
+    {
+      text: "Belajar state di React"
+    },
+    {
+      text: "Belajar React"
+    },
+    {
+      text: "Belajar React"
+    },
+  ];
+  
   return (
     <section className="todos">
-      <div className="todo">
-        <span className="todo-text">Learning React</span>
-      </div>
-      <div className="todo">
-        <span className="todo-text">Learning React</span>
-      </div>
-      <div className="todo">
-        <span className="todo-text">Learning React</span>
-      </div>
-      <div className="todo">
-        <span className="todo-text">Learning React</span>
-      </div>
-      <div className="todo">
-        <span className="todo-text">Learning React</span>
-      </div>
-      <div className="todo">
-        <span className="todo-text">Learning React</span>
-      </div>
-      <div className="todo">
-        <span className="todo-text">Learning React</span>
-      </div>
-      <div className="todo">
-        <span className="todo-text">Learning React</span>
-      </div>
-      <div className="todo">
-        <span className="todo-text">Learning React</span>
-      </div>
+      {todos.map(todo => {
+        return <Todo text={todo.text} />
+      })}
     </section>
   )
 }
