0e20aad45db1c421f34f984b189a56b87231b42e
Oktavian Yudistira Putra <oktavianyudistiraputra@gmail.com>
2021-09-28 23:45:26 +0700

    delete function & placeholder

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const Header = ({ showAddToggle, showAdd }) => {
+const Header = ({ showAddToggle, showAdd, clearTodos }) => {
   return (
     <section className="header">
       <button className="header-btn main-black-color" onClick={showAddToggle}>
@@ -9,14 +9,15 @@ const Header = ({ showAddToggle, showAdd }) => {
         {/* ternary operation */}
       </button>
       <h1 className="header-title">Todo Lists</h1>
-      <button className="header-btn main-red-color">Clear</button>
+      <button className="header-btn main-red-color" onClick={clearTodos}>Clear</button>
     </section>
   )
 }
 
 Header.propTypes = {
   showAddToggle: PropTypes.func.isRequired,
-  showAdd: PropTypes.bool.isRequired
+  showAdd: PropTypes.bool.isRequired,
+  clearTodos: PropTypes.func.isRequired,
 }
 
 export default Header;
--- src/pages/TodoList.jsx
+++ src/pages/TodoList.jsx
@@ -31,14 +31,14 @@ const TodoList = () => {
 
     setTodos(addedTodo)
   }
-
+  const clearTodos = () => setTodos([]);
   const showAddToggle = () => setShowAdd(!showAdd);
 
   console.log("todos", todos);
 
   return (
     <Paper>
-        <Header showAddToggle={showAddToggle} showAdd={showAdd} />
+        <Header showAddToggle={showAddToggle} showAdd={showAdd} clearTodos={clearTodos} />
 
         <TodoForm addTodo={addTodo} showAdd={showAdd} />
 
